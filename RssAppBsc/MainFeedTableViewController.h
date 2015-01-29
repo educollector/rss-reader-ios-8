0c7ce0981e623a09188a1d42d5c8440a4803f08f
Aleksandra Skierbiszewska <ola.skierbiszewska@norbsoft.com>
2015-01-29 13:22:46 +0100

    initial commit, prototype cell design

--- RssAppBsc/MainFeedTableViewController.h
+++ RssAppBsc/MainFeedTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  MainFeedTableViewController.h
 //  RssAppBsc
 //
 //  Created by Ola Skierbiszewska on 29/01/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface MainFeedTableViewController : UITableViewController
 
 @end
-
