ef766b5e5b414839e4fcec300eb7b3cad348e23a
Aleksandra Skierbiszewska <ola.skierbiszewska@norbsoft.com>
2015-01-30 12:47:12 +0100

    core data table "Url" added

---
+++ RssAppBsc/Url.h
@@ -0,0 +1,17 @@
+//
+//  Url.h
+//  RssAppBsc
+//
+//  Created by Ola Skierbiszewska on 30/01/15.
+//  Copyright (c) 2015 Ola Skierbiszewska. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Url : NSManagedObject
+
+@property (nonatomic, retain) NSString * url;
+
+@end
---
+++ RssAppBsc/Url.m
@@ -0,0 +1,16 @@
+//
+//  Url.m
+//  RssAppBsc
+//
+//  Created by Ola Skierbiszewska on 30/01/15.
+//  Copyright (c) 2015 Ola Skierbiszewska. All rights reserved.
+//
+
+#import "Url.h"
+
+
+@implementation Url
+
+@dynamic url;
+
+@end
