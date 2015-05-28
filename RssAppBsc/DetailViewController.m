cc44233f26d5f72be01271f63fbde936856ea517
amalicka <malicka.aleksandra@gmail.com>
2015-05-28 09:51:20 +0200

    web view - fully workin toolbar (back and forward buttons, disable/enable)

--- RssAppBsc/DetailViewController.m
+++ RssAppBsc/DetailViewController.m
@@ -15,11 +15,14 @@
 
 @implementation DetailViewController{
     NSURL *urlToLoad;
-    IBOutlet UIBarButtonItem *backButton;
-    IBOutlet UIBarButtonItem *forwardButton;
-    IBOutlet UIBarButtonItem *stopButton;
+    IBOutlet UIBarButtonItem *backBarButtonItem;
+    IBOutlet UIBarButtonItem *forwardBarButtonItem;
     IBOutlet UIToolbar *webViewToolbar;
 }
+
+//*****************************************************************************/
+#pragma mark - View methods
+//*****************************************************************************/
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -27,7 +30,23 @@
     [self uiMakeContent];
     [self uiNavigationBarStyling];
     [self uiMakeWebViewToolbar];
+    [self updateButtons];
 }
+
+- (void)viewWillDisappear:(BOOL)animated{
+    self.navigationController.toolbarHidden = YES;
+    self.navigationController.tabBarController.tabBar.hidden = NO;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+//*****************************************************************************/
+#pragma mark - Preparing layout
+//*****************************************************************************/
+
 -(void)uiMakeContent{
     self.webView.opaque = NO;
     self.webView.backgroundColor = [UIColor clearColor];    
@@ -39,27 +58,23 @@
 
 -(void)uiMakeWebViewToolbar{
     self.navigationController.toolbarHidden = NO;
-    self.tabBarController.tabBar.hidden = YES;
-   CGRect frame = CGRectMake(0, [[UIScreen mainScreen] bounds].size.height - 44-49, [[UIScreen mainScreen] bounds].size.width, 44);
-//    webViewToolbar = [[UIToolbar alloc]initWithFrame:frame];
-//    webViewToolbar.backgroundColor = [UIColor orangeColor];
-//    webViewToolbar.barStyle = UIBarStyleBlackTranslucent;
-//    [webViewToolbar sizeToFit];
-//    [self.view addSubview:webViewToolbar];
+    self.navigationController.tabBarController.tabBar.hidden  = YES;
     
     NSString *backArrowString = @"\U000025C0\U0000FE0E"; //BLACK LEFT-POINTING TRIANGLE PLUS VARIATION SELECTOR
-    NSString *forwardArrowString = @"\U000025BA\U0000FE0E"; //FORWARD LEFT-POINTING TRIANGLE PLUS VARIATION SELECTOR
+    NSString *forwardArrowString = @"\U000025B6\U0000FE0E"; //FORWARD LEFT-POINTING TRIANGLE PLUS VARIATION SELECTOR
     
-    UIBarButtonItem *backBarButtonItem = [[UIBarButtonItem alloc]
+    backBarButtonItem = [[UIBarButtonItem alloc]
                                           initWithTitle:backArrowString
                                           style:UIBarButtonItemStylePlain
                                           target:self
                                           action:@selector(webViewGoBack)];
-    UIBarButtonItem *forwardBarButtonItem = [[UIBarButtonItem alloc]
+    
+    forwardBarButtonItem = [[UIBarButtonItem alloc]
                                           initWithTitle:forwardArrowString
                                           style:UIBarButtonItemStylePlain
                                           target:self
                                           action:@selector(webViewGoForward)];
+    
     UIBarButtonItem *flexibleSpace = [[UIBarButtonItem alloc]
                                       initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace
                                       target:nil
@@ -72,23 +87,41 @@
                                 [[UIScreen mainScreen] bounds].size.height - self.navigationController.toolbar.frame.size.height,
                                 self.navigationController.toolbar.frame.size.width,
                                 self.navigationController.toolbar.frame.size.height);
+
 }
 
+//*****************************************************************************/
+#pragma mark - webView updating helper methods
+//*****************************************************************************/
+
+
 -(void)webViewGoBack{
-    if ([self.webView canGoBack])
-    {
-        [self.webView goBack];
-    }
+    [self.webView goBack];
 }
 
 -(void)webViewGoForward{
-    if ([self.webView canGoForward])
-    {
-        [self.webView canGoForward];
+    [self.webView canGoForward];
+}
+
+-(void)updateButtons{
+    forwardBarButtonItem.enabled = NO;
+    backBarButtonItem.enabled = NO;
+    if([self.webView canGoForward]){
+        forwardBarButtonItem.enabled = YES;
+    }
+    if([self.webView canGoBack]){
+        backBarButtonItem.enabled = YES;
     }
 }
-                                        
-                                          
+
+-(void)testMethod{
+    NSLog(@"I am a test method! I test nav bar buttons clicking!");
+}
+
+//*****************************************************************************/
+#pragma mark - Styling
+//*****************************************************************************/
+
 -(void)uiNavigationBarStyling{
     UIImage *shareImage = [[UIImage alloc] init];
     NSString *image = @"share.png";
@@ -105,18 +138,27 @@
     [self.navigationItem setRightBarButtonItems:barItemArray];
 }
 
--(void)testMethod{
-    NSLog(@"I am a test method! I test nav bar buttons clicking!");
+//*****************************************************************************/
+#pragma mark - UIWebView delegate methods
+//*****************************************************************************/
+- (void)webViewDidStartLoad:(UIWebView *)webView
+{
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+    [self updateButtons];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)webViewDidFinishLoad:(UIWebView *)webView
+{
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+    [self updateButtons];
 }
 
-#pragma mark UIWebView delegate methods
 - (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
     //if statemet lets to load all links on the website
+    
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+    [self updateButtons];
+    
     if(!([error code] == NSURLErrorCancelled)){
         NSLog(@"could not load the website caused by error: %@", error);
 
@@ -150,21 +192,11 @@
 
 - (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
     NSLog(@"shouldStartLoadWithRequest: %@", [[request URL] absoluteString]);
+    if (navigationType == UIWebViewNavigationTypeLinkClicked) {
+        NSLog(@"User tapped a link: %@", [[request URL] absoluteString]);
+    }
+    [self updateButtons];
     return YES;
-}
-- (void)webViewDidStartLoad:(UIWebView *)webView {
-    NSLog(@"webViewDidStartLoad");
-    
-}
-- (void)webViewDidFinishLoad:(UIWebView *)webView {
-    NSLog(@"webViewDidFinishLoad");
-    if (webView.isLoading){
-        NSLog(@"webview.isLoading");
-    }
-    else{
-        NSLog(@"webview NOT isLoading");
-    }
-    return;
 }
 
 /*
